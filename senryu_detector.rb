85302c524d7ac14de42d33e6b9a09e78f1a39c94
sa2taka <sa2taka@gmail.com>
2018-03-02 01:39:20 +0900

    追加: textから事前に削除する文字を削除する部分の追加

--- senryu_detector.rb
+++ senryu_detector.rb
@@ -1,9 +1,20 @@
 require 'natto'
 
 class SenryuDetector
+  attr_accessor :ignore_words, :delete_words, :permission_posids
+  def initialize
+    @delete_words, @ignore_words = read_file('exclude_word.tsv')
+    @permission_posids, * = read_file('permission.tsv')
+    @permission_posids.map!(&:to_i)
+  end
+
   def senryu?(text)
-    pronunciations(text).each do |pron|
-      p pron
+    safe_text = delete_excludes(text)
+    pronunciations(safe_text).each do |parsed|
+      if parsed.is_bos? || parsed.is_eos?
+        break
+      end
+      puts "#{parsed.posid}: #{parsed.surface}(#{parsed.feature})"
     end
   end
 
@@ -11,6 +22,27 @@ class SenryuDetector
     pronunciation_nm = Natto::MeCab.new('-F%f[8]')
     pronunciation_nm.enum_parse(text)
   end
+
+  private
+
+  def read_file(filename)
+    reading_line = []
+    File.open(filename) do |f|
+      f.each_line do |line|
+        reading_line << line.split[1..-1]
+      end
+    end
+    reading_line
+  end
+
+  def delete_excludes(text)
+    dump = text.dup
+    @delete_words.each do |excluded|
+      dump.delete!(excluded)
+    end
+    dump
+  end
 end
 
-SenryuDetector.new.senryu?("柿食えば\n　鐘がなるなり\n　　法隆寺")
+text = "乾杯"
+SenryuDetector.new.senryu?(text)

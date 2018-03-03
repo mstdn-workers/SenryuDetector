c76f82c87a8bcd7e55967f836d7407e55c590200
sa2taka <sa2taka@gmail.com>
2018-03-03 23:40:25 +0900

    追加: streamingで取得したtootを川柳か判定できるように(Stremingが黙って死ぬ

--- main.rb
+++ main.rb
@@ -1,2 +1,62 @@
 require './register'
 require './senryu_detector'
+require './stream'
+require 'json'
+require 'pp'
+
+class Main
+  include Register
+
+  def initialize
+    @client = init_app
+    @detector = SenryuDetector.new
+    @stream = MstdnStream.new.stream
+
+    set_stream
+  end
+
+  private
+
+  def set_stream
+    detector = @detector
+    @stream.on :message do |msg|
+      if msg.data.size > 0
+        p msg.data
+        begin
+          remove_tag = -> (str) { str.gsub(/<([^>]+)>/, '') }
+          info = JSON.parse(msg.data)
+          if info['event'] == 'update'
+            body = JSON.parse(info['payload'])
+            p remove_tag.call(body['content'])
+            p detector.senryu?(remove_tag.call(body['content']))
+          end
+        rescue => e
+          p e
+        end
+      end
+    end
+
+    @stream.on :open do
+      puts "streaming open"
+    end
+
+    @stream.on :close do |e|
+      puts "close"
+      p e
+      exit 1
+    end
+
+    @stream.on :error do |e|
+      p e
+    end
+  end
+end
+
+begin
+  Main.new
+  loop do
+    sleep(1)
+  end
+rescue => e
+  p e
+end
--- stream.rb
+++ stream.rb
@@ -5,14 +5,8 @@ class MstdnStream
   TOKEN_FILE_NAME = '.access_token'.freeze
   ACCESS_TOKEN = File.read(TOKEN_FILE_NAME).chomp.freeze
 
-  def initialize
+  def stream
     url = "#{STREAMING_PATH}?access_token=#{ACCESS_TOKEN}&stream=public:local"
-    p url
     WebSocket::Client::Simple.connect(url)
   end
 end
-
-# stream.on :message do |msg|
-#   puts '!message'
-#   p msg.data
-# end
